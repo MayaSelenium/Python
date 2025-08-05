19472c581046ec9f80ef110ae6ae1f0333ccb05d
MayaSelenium <zoloto18081991@mail.ru>
2025-08-06 00:03:20 +0300

    hw_2

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -1,10 +1,5 @@
-class User:
-    def __init__(self, first_name, last_name):
-        self.myname=first_name
-        self.mylast_name=last_name
-    def MN(self):
-        print("Имя",self.myname )
-    def MLN(self):
-        print("Фамилия", self.mylast_name)
-    def MNMLN(self):
-        print("Имя и фамилия",self.myname,self.mylast_name)
+from lesson_3_task_1 import User
+my_user=User("Maya","Tereschenko")
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
